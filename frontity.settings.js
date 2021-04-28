b2614beb07b480cd30bd23163429a96fb92caee0
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2021-04-28 15:56:19 +0200

    Test again

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,58 +1,43 @@
 const settings = {
-  "name": "nkjt-test",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
-    }
+  name: "nkjt-test",
+  state: {
+    frontity: {
+      url: "http://localhost:8888/",
+      title: "Test Frontity Blog",
+      description: "WordPress installation for Frontity development",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Nature", "/category/nature/"],
+            ["Travel", "/category/travel/"],
+            ["Japan", "/tag/japan/"],
+            ["About Us", "/about-us/"],
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
           // "url": "https://test.frontity.org"
-          "url": "http://localhost:8888/"
-        }
-      }
+          url: "http://localhost:8888/",
+        },
+      },
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    "@frontity/html2react",
+  ],
 };
 
 export default settings;
